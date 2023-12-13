c96337821232e0a2404239775e5303907e5f2b15
Sanglafort <silviaangfort@gmail.com>
2023-12-13 13:11:41 +0100

    Component escena OK

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-escena',
   standalone: true,
   imports: [],
-  templateUrl: './escena.component.html',
+  template: `<h3>Dedica moltes hores</h3>
+
+  <p>Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.</p>`,
   styleUrl: './escena.component.css'
 })
 export class EscenaComponent {
